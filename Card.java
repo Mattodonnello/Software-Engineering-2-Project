9b4a21f6b5148f01e3920b18de7b5451fc13164d
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2021-03-16 13:37:06 +0000

    Creation of Card Class

---
+++ Card.java
@@ -0,0 +1,20 @@
+public class Card {
+	
+	private int countryId;
+	private String countryName;
+
+	Card (int inCountryId, String inCountryName) {   
+		countryId = inCountryId;
+		countryName = inCountryName;
+		return;
+	}
+	
+	public int getCountryId () {
+		return countryId;
+	}
+	
+	public String getCountryName () {
+		return countryName;
+	}
+	
+}
