6f998de15a85047cd8550bb19386e82090535bde
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2021-03-16 15:54:37 +0000

    Add errors and extra country function
    Add functions to detect errors other than incorrect country name and also add an extra countryID function to distinguish between neutrals and players

--- Parse.java
+++ Parse.java
@@ -1,8 +1,9 @@
-
 public class Parse {
 	
 	private String[] countryCodes = new String[GameData.NUM_COUNTRIES];
 	private boolean isError = false;
+	private boolean isError2 = false;
+	private boolean isError3 = false;
 	private int countryId = 0;
 	private boolean isDone = false;
 	private int numUnits;
@@ -12,7 +13,7 @@ public class Parse {
 		for (int i=0; i<GameData.NUM_COUNTRIES; i++) {
 			name = GameData.COUNTRY_NAMES[i];
 			name = name.toLowerCase();
-			name = name.replaceAll("\\s","");
+			name = name.replaceAll("\\s",""); // removes all the occurrences of white spaces
 			name = name.substring(0,4);
 			countryCodes[i] = name;
 		}
@@ -20,6 +21,44 @@ public class Parse {
 	}
 	
 	public void countryId (String string) {
+		boolean found = false;
+		boolean found2 = true;
+		boolean found3 = true;
+		String number= string.replaceAll("[^0-9]", "");
+		int num = Integer.parseInt(number);
+		
+		if(num>3) {
+			found3 = false; 
+			found2=true;
+			found = true;
+		}
+		else if (!(string.contains("3"))) {
+		found2 = false;	
+		found= true;
+		numUnits = 3;
+		}
+		else {
+		numUnits = num;
+		string = string.replaceAll("\\s", "");
+		string = string.replaceAll("\\d","");
+		if (string.length() >= 4) {
+			string = string.toLowerCase();
+			string = string.substring(0,4);
+	 		for (int i=0; (i<GameData.NUM_COUNTRIES) && !found; i++) {
+				if (string.equals(countryCodes[i])) {
+					found = true;
+					countryId = i;
+				}
+	 		}
+		}
+		}
+		isError = !found;
+		isError2 = !found2;
+		isError3 = !found3;
+		return;
+	}
+	
+	public void countryId2 (String string) {
 		boolean found = false;
 		string = string.replaceAll("\\s", "");
 		if (string.length() >= 4) {
@@ -38,6 +77,14 @@ public class Parse {
 	
 	public boolean isError () {
 		return isError;
+	}
+	
+	public boolean isError2 () {
+		return isError2;
+	}
+	
+	public boolean isError3 () {
+		return isError3;
 	}
 	
 	public boolean isDone () {
