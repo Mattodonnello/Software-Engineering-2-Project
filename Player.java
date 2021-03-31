45b556d4502f8e8b9b8dce31bff4691b2a60c9da
PearseFrost <63857939+PearseFrost@users.noreply.github.com>
2021-03-31 14:02:43 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,56 +1,72 @@
-package Run;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Player {
-	
-	private int id;
-	private String name;
-	private int numUnits;
-	private ArrayList<Integer> dice = new ArrayList<Integer>();
-	
-	Player (int inId, String inName, int inNumUnits) {
-		id = inId;
-		name = inName;
-		numUnits = inNumUnits;
-		return;
-	}
-	
-	public void rollDice (int numDice) {
-		dice.clear();
-		for (int j=0; j<numDice; j++) {
-				dice.add(1 + (int)(Math.random() * 6));   
-		}
-		return;
-	}
 
-	public void addUnits (int inNum) {
-		numUnits = numUnits + inNum;
-		return;
-	}
-	
-	public void subtractUnits (int inNum) {
-		numUnits = numUnits - inNum;
-		return;
-	}
-		
-	public int getId () {
-		return id;
-	}
-	
-	public String getName () {
-		return name;
-	}
-	
-	public int getNumUnits () {
-		return numUnits;
-	}
+    private int id;
+    private String name;
+    private int numUnits;
+    private ArrayList<Integer> dice = new ArrayList<Integer>();
+    private int battleLoss = 0;
 
-	public ArrayList<Integer> getDice () {
-		return dice;
-	}
-	
-	public int getDie (int dieId) {
-		return dice.get(dieId);
-	}
-	
+    Player (int inId, String inName, int inNumUnits) {
+        id = inId;
+        name = inName;
+        numUnits = inNumUnits;
+        return;
+    }
+
+    public void rollDice (int numDice) {
+        dice.clear();
+        for (int j=0; j<numDice; j++) {
+            dice.add(1 + (int)(Math.random() * 6));
+        }
+        Collections.sort(dice, Collections.reverseOrder());
+        return;
+    }
+
+    public void addUnits (int inNum) {
+        numUnits = numUnits + inNum;
+        return;
+    }
+
+    public void subtractUnits (int inNum) {
+        numUnits = numUnits - inNum;
+        return;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public int getNumUnits () {
+        return numUnits;
+    }
+
+    public ArrayList<Integer> getDice () {
+        return dice;
+    }
+
+    public int getDie (int dieId) {
+        return dice.get(dieId);
+    }
+
+    public void resetBattleLoss () {
+        battleLoss = 0;
+        return;
+    }
+
+    public void addBattleLoss () {
+        battleLoss++;
+        return;
+    }
+
+    public int getBattleLoss () {
+        return battleLoss;
+    }
+
 }
