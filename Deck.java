4465bd92d4b2b582417833ed5b2e1dbfe5c2391c
PearseFrost <63857939+PearseFrost@users.noreply.github.com>
2021-03-31 18:35:59 +0100

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 
 public class Deck {
-
+	
+        private String[] typeOfCard;
 	private ArrayList<Card> cards;
 	
 	Deck () {
 		int cardId;
+		typeOfCard = new String[]{ "Infantry", "Cavalry", "Artillery"};
 		cards = new ArrayList<Card>();
 		for (cardId=0; cardId<GameData.NUM_COUNTRIES; cardId++) {
 			cards.add(new Card(cardId, GameData.COUNTRY_NAMES[cardId]));
 		}
-		return;
+		Collections.shuffle(cards);
 	}
 	
 	public Card getCard () {
@@ -18,9 +20,15 @@ public class Deck {
 		Card card = cards.remove(index);
 		return card;
 	}
+	public void addCard(Card card){
+        cards.add(card);
+    }
+	
 	
 	public boolean isEmpty () {
 		return cards.isEmpty();
 	}
-	
+	 public void shuffleCards(){
+        Collections.shuffle(cards);
+    }
 }
